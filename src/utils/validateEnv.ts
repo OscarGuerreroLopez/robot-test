68058b9ee6688dc811998338437b0dc97c95a65e
Oscar Guerrero Lopez <oscar.computer.guy@gmail.com>
2020-12-10 11:49:27 +0100

    changing default port

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -3,7 +3,7 @@ import { cleanEnv, str, num } from "envalid";
 const getEnvVars = (): EnvObject => {
   const EnvVars = cleanEnv(process.env, {
     NODE_ENV: str(),
-    PORT: num({ default: 5000 }),
+    PORT: num({ default: 8007 }),
     REDIS_PORT: num({ default: 6379 }),
     REDIS_HOST: str({ default: "localhost" }),
   });
